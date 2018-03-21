ff7f4c8ce072248f43eeeb1c9aa46cc15bd5dd27
liqi <lq@mpig.com.cn>
2018-03-21 18:36:23 +0800

    improvement
    some improvements in RRT

---
+++ RRT/test.m
@@ -0,0 +1,14 @@
+% I = [10,1,0;0,1,7;3,5,9];
+% a = double([I]);
+% b = rand -1;
+% c = I(:,1:2);
+% d = [1,2];
+% e = [1,3];
+% f = distanceCost(d,e)
+% g = min
+% if rand < 0.5, 
+%         sample = rand(1,2) .* size(I);
+% else
+%     sample = b;
+% end
+map=imread('map1.bmp');
